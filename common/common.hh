27734af9d2a466843483d81a988b14022d2a8c5a
pingul <joel@wbn.se>
2016-12-09 13:16:21 +0100

    Adding comment.

--- common/common.hh
+++ common/common.hh
@@ -1,6 +1,12 @@
 #include <istream>
 #include <string>
 
+/*
+ * Makes it possible to write
+ *
+ *   T a, b;
+ *   std::cin >> skip >> a >> b;
+*/
 template <typename T = std::string, typename CharT = char>
 std::basic_istream<CharT>& skip(std::basic_istream<CharT>& in) 
 {
