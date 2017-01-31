6b29f1f3d58d7fcfb9008a44fa4e1af4951863b6
Joel Wretborn <swretbor@macbe16657.dyndns.cern.ch>
2017-01-31 15:01:36 +0100

    tabs -> spaces

--- 2d-synchrotron/main.cc
+++ 2d-synchrotron/main.cc
@@ -9,30 +9,30 @@ namespace jwc {
 template <typename TModel>
 void generateLossmap(typename TModel::RAMP_TYPE type)
 {
-	TModel tm(5000, type, typename TModel::LossAnalysis());
-	auto freq = TModel::Accelerator::getLHC().revolution_freq;
-	int turns = 70*freq;
-	tm.takeTimesteps(turns); 
-	tm.writeCollHits(COLL_FILE);
+    TModel tm(1000, type, typename TModel::LossAnalysis());
+    auto freq = TModel::Accelerator::getLHC().revolution_freq;
+    int turns = 70*freq;
+    tm.takeTimesteps(turns); 
+    tm.writeCollHits(COLL_FILE);
 
-	int ilastHit, tlastHit = -1;
-	auto hits = tm.getCollHits();
-	for (int i = 0; i < hits.size(); ++i) {
-		if (hits[i] > tlastHit) {
-			tlastHit = hits[i];
-			ilastHit = i;
-		}
-	}
+    int ilastHit, tlastHit = -1;
+    auto hits = tm.getCollHits();
+    for (int i = 0; i < hits.size(); ++i) {
+        if (hits[i] > tlastHit) {
+            tlastHit = hits[i];
+            ilastHit = i;
+        }
+    }
 
-	if (tlastHit == -1)
-		std::cout << "No losses" << std::endl;
-	else {
-		auto energy = tm.getEnergy();
-		std::cout 
-			<< "Latest hit:\n\tparticle " << ilastHit << ", turn " << tlastHit 
-			<< "(approx. after " << std::setprecision(3) << (double(tlastHit)/freq) << " s)\n"
-			<< "\tstarting energy " << std::setprecision(5) << energy[ilastHit] << std::endl;
-	}
+    if (tlastHit == -1)
+        std::cout << "No losses" << std::endl;
+    else {
+        auto energy = tm.getEnergy();
+        std::cout 
+            << "Latest hit:\n\tparticle " << ilastHit << ", turn " << tlastHit 
+            << "(approx. after " << std::setprecision(3) << (double(tlastHit)/freq) << " s)\n"
+            << "\tstarting energy " << std::setprecision(5) << energy[ilastHit] << std::endl;
+    }
 
 }
 
@@ -40,47 +40,47 @@ void generateLossmap(typename TModel::RAMP_TYPE type)
 
 int main(int argc, char* argv[])
 {
-	std::vector<std::string> args(argv, argv + argc);
+    std::vector<std::string> args(argv, argv + argc);
 
-	tbb::task_scheduler_init init(4);
+    tbb::task_scheduler_init init;
 
-	typedef jwc::ToyModel<double> ToyModel;
+    typedef jwc::ToyModel<double> ToyModel;
 
-	// CHANGE FOR DIFFERENT SIMULATIONS
-	ToyModel::RAMP_TYPE type = ToyModel::LHC_RAMP;
+    // CHANGE FOR DIFFERENT SIMULATIONS
+    ToyModel::RAMP_TYPE type = ToyModel::LHC_RAMP;
 
-	if (args.size() < 2) {
-		std::cout << "Not enough arguments specified" << std::endl;
-	} else if (args[1] == "animate") {
-		{
-			std::cout << "Creating particle paths" << std::endl;
-			ToyModel tm(1000, type);
-			tm.takeTimesteps(5000, jwc::PATH_FILE, 10);
-			tm.writeCollHits(jwc::COLL_FILE);
-		}
-		{
-			std::cout << "Creating line segments" << std::endl;
-			ToyModel tm(type, ToyModel::LineSim());
-			tm.takeTimesteps(1700, jwc::LINE_FILE, 5);
-		}
-	} else if (args[1] == "energy") {
-		std::cout << "Simulating 1 particle" << std::endl;
-		ToyModel tm(1, type);
-		tm.takeTimesteps(40000, jwc::PATH_FILE);
-		tm.writeCollHits(jwc::COLL_FILE);
-	} else if (args[1] == "lossmap-analysis" || args[1] == "lossmap") {
-		std::cout << "Loss pattern analysis" << std::endl;
-		jwc::generateLossmap<ToyModel>(type);
-	} else if (args[1] == "sixtrack-comp") {
-		std::cout << "Sixtrack comparison" << std::endl;
-		ToyModel tm( (ToyModel::SixTrackTest()) );
-		tm.takeTimesteps(30000, jwc::SIXTRACK_TEST_FILE);
-	} else if (args[1] == "startdist") {
-		std::cout << "Start distribution" << std::endl;
-		ToyModel tm(5000, type, ToyModel::LossAnalysis());
-	} else {
-		std::cout << "No action with name '" << args[1] << "' found" << std::endl;
-	}
+    if (args.size() < 2) {
+        std::cout << "Not enough arguments specified" << std::endl;
+    } else if (args[1] == "animate") {
+        {
+            std::cout << "Creating particle paths" << std::endl;
+            ToyModel tm(1000, type);
+            tm.takeTimesteps(5000, jwc::PATH_FILE, 10);
+            tm.writeCollHits(jwc::COLL_FILE);
+        }
+        {
+            std::cout << "Creating line segments" << std::endl;
+            ToyModel tm(type, ToyModel::LineSim());
+            tm.takeTimesteps(1700, jwc::LINE_FILE, 5);
+        }
+    } else if (args[1] == "energy") {
+        std::cout << "Simulating 1 particle" << std::endl;
+        ToyModel tm(1, type);
+        tm.takeTimesteps(40000, jwc::PATH_FILE);
+        tm.writeCollHits(jwc::COLL_FILE);
+    } else if (args[1] == "lossmap-analysis" || args[1] == "lossmap") {
+        std::cout << "Loss pattern analysis" << std::endl;
+        jwc::generateLossmap<ToyModel>(type);
+    } else if (args[1] == "sixtrack-comp") {
+        std::cout << "Sixtrack comparison" << std::endl;
+        ToyModel tm( (ToyModel::SixTrackTest()) );
+        tm.takeTimesteps(30000, jwc::SIXTRACK_TEST_FILE);
+    } else if (args[1] == "startdist") {
+        std::cout << "Start distribution" << std::endl;
+        ToyModel tm(5000, type, ToyModel::LossAnalysis());
+    } else {
+        std::cout << "No action with name '" << args[1] << "' found" << std::endl;
+    }
 
-	return 0;
+    return 0;
 }
