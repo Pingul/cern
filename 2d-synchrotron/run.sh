7273c20c8e81ff9f0e0da49e7185457e73784eeb
pingul <joel@wbn.se>
2017-02-01 09:58:17 +0100

    tabs -> spaces

--- 2d-synchrotron/run.sh
+++ 2d-synchrotron/run.sh
@@ -1,6 +1,7 @@
 
-possibleActions="animate lossmap energy lossmap-analysis startdist"
+possibleActions="animate lossmap energy lossmap-analysis startdist phasespace"
 action="$1"
+arg="$2"
 save_file="$2"
 
 workingdir=`pwd`
@@ -11,29 +12,29 @@ cache="$workingdir/simulations/$(date +%Y.%m%.%d.%H.%M.%S)"
 actionOK=0
 for a in $possibleActions
 do
-	if [ "$a" == "$action" ]; then
-		actionOK=1;
-	fi
+    if [ "$a" == "$action" ]; then
+        actionOK=1;
+    fi
 done
 
 if [ "$actionOK" == 0 ]; then
-	echo " --- $action is not a valid action, choose from "
-	echo " --- \t$possibleActions"
+    echo " --- $action is not a valid action, choose from "
+    echo " --- \t$possibleActions"
 else
-	echo " --- Running '$action'..."
-	if [ ${#save_file} -gt 0 ]; then
-		echo " --- Will save potential output to '$save_file'."
-	else
-		echo " --- Will not save data."
-	fi
+    echo " --- Running '$action'..."
+    if [ ${#save_file} -gt 0 ]; then
+        echo " --- Will save potential output to '$save_file'."
+    else
+        echo " --- Will not save data."
+    fi
 
-	echo " --- Compiling c++... "
-	make 
-	echo " --- Running c++... "
-	echo " --- Caching data in '$cache'."
-	mkdir $cache
-	./main $action | tee $cache/stdout.txt
-	cp $resultdir/* $cache
-	echo " --- Plotting...  "
-	python3.5 plot.py $action $save_file
+    echo " --- Compiling c++... "
+    make 
+    echo " --- Running c++... "
+    echo " --- Caching data in '$cache'."
+    mkdir $cache
+    ./main $action $arg | tee $cache/stdout.txt
+    cp $resultdir/* $cache
+    echo " --- Plotting...  "
+    python3.5 plot.py $action $save_file
 fi
