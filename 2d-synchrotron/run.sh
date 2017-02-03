29565bfaddd8af619faaf5bb98d19ad0e4582c6b
pingul <joel@wbn.se>
2017-02-03 15:43:22 +0100

    Smaller changes that made it possible to plot evolution of phase space

--- 2d-synchrotron/run.sh
+++ 2d-synchrotron/run.sh
@@ -2,12 +2,11 @@
 possibleActions="animate lossmap energy lossmap-analysis startdist phasespace"
 action="$1"
 arg="$2"
-save_file="$2"
 
 workingdir=`pwd`
 resultdir="$workingdir/calc"
 moviedir="$workingdir/movies"
-cache="$workingdir/simulations/$(date +%Y.%m%.%d.%H.%M.%S)"
+cache="$workingdir/simulations/cache/$(date +%Y.%m%.%d.%H.%M.%S)"
 
 
 actionOK=0
@@ -22,32 +21,26 @@ if [ "$actionOK" == 0 ]; then
     echo " --- $action is not a valid action, choose from "
     echo " --- \t$possibleActions"
 else
-    echo " --- Running '$action'..."
-    if [ ${#save_file} -gt 0 ]; then
-        echo " --- Will save potential output to '$save_file'."
-    else
-        echo " --- Will not save data."
-    fi
-
+    echo " --- Running '$action $arg'..."
     echo " --- Compiling c++... "
     make 
     echo " --- Running c++... "
-    if [ "$action" == "phasespace" ]; then
-        for (( i=500; i <= 5000000; i+=11245 ))
-        do
-            echo " --- Turn $i "
-            ./main $action $i
-            echo " --- Saving image to '$moviedir/ramp_turn$i.png"
-            python3.5 plot.py $action $moviedir/ramp_turn$i.png
-            #echo " --- Copying result to '$resultdir/lines$i.dat'"
-            #mv $resultdir/lines.dat $resultdir/lines$i.dat
-        done
-    else
+    #if [ "$action" == "phasespace" ]; then
+        #for (( i=500; i <= 5000000; i+=11245 ))
+        #do
+            #echo " --- Turn $i "
+            #./main $action $i
+            #echo " --- Saving image to '$moviedir/ramp_turn$i.png"
+            #python3.5 plot.py $action $moviedir/ramp_turn$i.png
+            ##echo " --- Copying result to '$resultdir/lines$i.dat'"
+            ##mv $resultdir/lines.dat $resultdir/lines$i.dat
+        #done
+    #else
         echo " --- Caching data in '$cache'."
         mkdir $cache
         ./main $action $arg | tee $cache/stdout.txt
         cp $resultdir/* $cache
         echo " --- Plotting...  "
-        python3.5 plot.py $action $save_file
-    fi
+        python3.5 plot.py $action $arg
+    #fi
 fi
