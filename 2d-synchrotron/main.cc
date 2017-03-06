1591dd91c9f010812281231a6c778d6b01275cb3
pingul <joel@wbn.se>
2017-03-06 11:05:02 +0100

    Having some changes here reflection the recent changes in synchrotron.hh as well

--- 2d-synchrotron/main.cc
+++ 2d-synchrotron/main.cc
@@ -5,6 +5,7 @@
 #include <tbb/task_scheduler_init.h>
 #include "2d-synchrotron.hh"
 
+
 int main(int argc, char* argv[])
 {
     std::vector<std::string> args(argv, argv + argc);
@@ -20,9 +21,9 @@ int main(int argc, char* argv[])
         std::cout << "Not enough arguments specified" << std::endl;
     } else if (args[1] == "animate") {
         std::cout << "Short animation" << std::endl;
-        ToyModel tm(10, type, ToyModel::LossAnalysis());
-        //tm.simulateTurns(5000, twodsynch::PATH_FILE, 10);
-        tm.simulateTurns(300, twodsynch::PATH_FILE, 1);
+        //ToyModel tm(10, type, ToyModel::LossAnalysis());
+        ToyModel tm(type, ToyModel::LossAnalysisAction2());
+        tm.simulateTurns(1000, twodsynch::PATH_FILE, 1);
         tm.writeCollHits(twodsynch::COLL_FILE);
         writePhasespaceFrame(ToyModel::Accelerator::getLHC(), twodsynch::LINE_FILE);
     } else if (args[1] == "animate-long") {
@@ -47,9 +48,8 @@ int main(int argc, char* argv[])
         tm.writeCollHits(twodsynch::COLL_FILE);
     } else if (args[1] == "lossmap-analysis" || args[1] == "lossmap") {
         std::cout << "Loss pattern analysis" << std::endl;
-        //ToyModel lossmodel(type, ToyModel::LossAnalysisAction());
-        ToyModel lossmodel(10000, type, ToyModel::LossAnalysis());
-        //ToyModel lossmodel("test_lost.dat", type, ToyModel::LossAnalysisMultiplied());
+        ToyModel lossmodel(type, ToyModel::LossAnalysisAction());
+        //ToyModel lossmodel(500, type, ToyModel::LossAnalysis());
         lossmodel.runLossmap(20);
     } else if (args[1] == "sixtrack-comp") {
         std::cout << "Sixtrack comparison" << std::endl;
@@ -58,7 +58,6 @@ int main(int argc, char* argv[])
     } else if (args[1] == "startdist") {
         std::cout << "Start distribution" << std::endl;
         ToyModel lossmodel(type, ToyModel::LossAnalysisAction());
-        //ToyModel lossmodel("test_lost.dat", type, ToyModel::LossAnalysisMultiplied());
         //ToyModel tm(2000, type, ToyModel::LossAnalysis());
     } else if (args[1] == "phasespace") {
         writePhasespaceFrame(ToyModel::Accelerator::getLHC(), twodsynch::LINE_FILE);
@@ -69,7 +68,7 @@ int main(int argc, char* argv[])
             std::cout << "Must provide file path" << std::endl;
         else {
             ToyModel tm(args[2], type);
-            tm.simulateTurns(600, twodsynch::PATH_FILE, 5);
+            tm.simulateTurns(600, twodsynch::PATH_FILE, 1);
             //tm.runLossmap(20);
             //tm.simulateTurns(20*11245, twodsynch::PATH_FILE, 4);
         }
@@ -79,9 +78,34 @@ int main(int argc, char* argv[])
         tm.writeCollHits(twodsynch::COLL_FILE);
         tm.writeLostTurns("calc/lost.dat");
     } else if (args[1] == "test") {
+        using twodsynch::cnst::pi;
+        using twodsynch::levelCurve;
+        using twodsynch::hamiltonian;
+
         auto acc = twodsynch::Accelerator<double>::getLHC();
-        acc.setE(7000e9, true);
-        std::cout << "Default rev frequency: " << acc.f_rev << std::endl;
+        std::cout << acc.lag_phase() << std::endl;
+        //const double Hsep = -twodsynch::separatrix(acc);
+        const double Hsep = 1e8;
+        std::cout << "separatrix H = " << Hsep << std::endl;
+        for (double ph = 0; ph < 2.0*pi; ph += 0.2*pi) {
+            const double de = twodsynch::levelCurve(acc, ph, Hsep);
+            const double H = twodsynch::hamiltonian(acc, de, ph);
+            std::cout << "H(" << ph << ", " << de << ") = " << H << std::endl;
+        }
+    } else if (args[1] == "test2") {
+        using twodsynch::cnst::pi;
+        using twodsynch::levelCurve;
+        using twodsynch::hamiltonian;
+
+
+        auto acc = twodsynch::Accelerator<double>::getLHC();
+        const double de = -1.90986e+08;
+        std::cout << "de = " << de << std::endl;
+        for (double ph = 0; ph < 2.0*pi; ph += 0.2*pi) {
+            const double H = twodsynch::hamiltonian(acc, de, ph);
+            const double dde = twodsynch::levelCurve(acc, ph, H);
+            std::cout << "H-1(" << ph << ", " << H << ") = " << dde << std::endl;
+        }
     } else {
         std::cout << "No action with name '" << args[1] << "' found" << std::endl;
     }
