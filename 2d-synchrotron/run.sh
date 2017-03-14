c5fb32b5ee7d34920e3a9e224611634d916ef538
pingul <joel@wbn.se>
2017-03-14 13:27:34 +0100

    Fixing issue where the stdcout.txt file was not cached correctly

--- 2d-synchrotron/run.sh
+++ 2d-synchrotron/run.sh
@@ -25,22 +25,11 @@ else
     echo " --- Compiling c++... "
     make 
     echo " --- Running c++... "
-    #if [ "$action" == "phasespace" ]; then
-        #for (( i=500; i <= 5000000; i+=11245 ))
-        #do
-            #echo " --- Turn $i "
-            #./main $action $i
-            #echo " --- Saving image to '$moviedir/ramp_turn$i.png"
-            #python3.5 plot.py $action $moviedir/ramp_turn$i.png
-            ##echo " --- Copying result to '$resultdir/lines$i.dat'"
-            ##mv $resultdir/lines.dat $resultdir/lines$i.dat
-        #done
-    #else
-        echo " --- Caching data in '$cache'."
-        mkdir $cache
-        ./main $action $arg | tee $cache/stdout.txt
-        cp $resultdir/* $cache
-        echo " --- Plotting...  "
-        python3.5 plot.py $action $arg
-    #fi
+    echo " --- Caching data in '$cache'."
+
+    mkdir $cache
+    ./main $action $arg | tee $resultdir/stdout.txt
+    cp $resultdir/* $cache
+    echo " --- Plotting...  "
+    python3.5 plot.py $action $arg
 fi
