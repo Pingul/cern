4bc62841627c470c39e603083164b0e07469e7ed
pingul <joel@wbn.se>
2017-04-06 15:13:19 +0200

    Adding log colors

--- common/logger.py
+++ common/logger.py
@@ -1,24 +1,44 @@
+# encoding: utf-8
 from enum import Enum
 
+LogLevel = Enum("LogLevel", "info success notify warning")
 
-LogLevel = Enum("LogLevel", "info warning")
+def color_wrap(string, color):
+    return '\x1b[{}m'.format(color) + string + '\x1b[0m'
 
 class ModuleLogger:
+    colors = {
+            LogLevel.notify : '1;36;40',
+            LogLevel.warning : '1;31;40',
+            LogLevel.success : '1;32;40'
+            }
 
     def __init__(self, module_name):
         self.module_name = module_name
 
     def log(self, *args, log_level=LogLevel.info, end='\n', module_prestring=True):
-        if log_level == LogLevel.warning:
-            print("*WARNING*", end=" ")
-        if module_prestring: 
-            print(self.module_name, end=": ")
-        print(*args, end=end)
+        
+        message = " ".join([*args])
+        if module_prestring:
+            message = "{}: {}".format(self.module_name, message)
+            if log_level == LogLevel.warning:
+                message = "*WARNING* {}".format(message)
+        
+        if log_level in self.colors:
+            message = color_wrap(message, self.colors[log_level])
+
+        print(message, end=end)
 
 if __name__ == "__main__":
+    # Some testcases
     lg = ModuleLogger("Test")
-    lg.log("testing message info", LogLevel.info)
-    lg.log("testing message warning", LogLevel.warning)
+    lg.log("message info", log_level=LogLevel.info)
+    lg.log("message success", log_level=LogLevel.success)
+    lg.log("message notify", log_level=LogLevel.notify)
+    lg.log("message warning", log_level=LogLevel.warning)
     lg.log("testing default message")
     lg.log("testing", "many", "messages")
 
+    lg.log("message", end=' ')
+    lg.log("that", end=' ', module_prestring=False)
+    lg.log("continues", module_prestring=False)
