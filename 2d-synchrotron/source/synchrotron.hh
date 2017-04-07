5650d894fcee349397300f40151fe54f0c42eacc
pingul <joel@wbn.se>
2017-04-07 11:17:35 +0200

    Removing all different constructors -- it is fixed by the particles.hh code now

--- 2d-synchrotron/source/synchrotron.hh
+++ 2d-synchrotron/source/synchrotron.hh
@@ -121,207 +121,35 @@ struct SimpleSynchrotron
     SimpleSynchrotron(ParticlesPtr particles, Acc acc, RAMP_TYPE ramp)
         : mAcc(acc), mParticles(particles), mType(ramp)
     {
-        initStorage(particles->size());
+        mCollHits.assign(mParticles->size(), -1);
         writeSingleDistribution(STARTDIST_FILE);
     }
-
-    SimpleSynchrotron(ParticlesPtr particles, RAMP_TYPE type)
-        : mAcc(Acc::getLHC()), mParticles(particles), mType(type)
-    {
-        initStorage(mParticles->size());
-        writeSingleDistribution(STARTDIST_FILE);
-    }
-
-    SimpleSynchrotron(size_t n, RAMP_TYPE type)
-        : mAcc(Acc::getLHC()), mParticles(ParticlesPtr(new Particles(n))), mType(type)
-    {
-        initStorage(n);
-
-        std::random_device rdev;
-        std::mt19937 generator(rdev());
-
-        std::normal_distribution<> e_dist(0, 0.20e9);
-        std::normal_distribution<> ph_dist(cnst::pi, cnst::pi/4);
-        for (size_t i = 0; i < n; ++i) {
-            const T deltaE = e_dist(generator);
-            const T phase = ph_dist(generator);
-            mParticles->momentum[i] = deltaE;
-            mParticles->phase[i] = phase;
-        }
-        writeSingleDistribution(STARTDIST_FILE);
-    }
-
-
-    // For parameter passing in the next constructors
-    // These are all different distributions to choose from
-    struct AroundSeparatrix {};
-    struct ActionValues {};
-    struct LinearDecay {};
-    struct ExponentialDecay {};
-    
-    struct SixTrackTest {};
-
-    SimpleSynchrotron(int n, RAMP_TYPE type, AroundSeparatrix)
-        : mAcc(Acc::getLHC()), mParticles(ParticlesPtr(new Particles(n))), mType(type)
-    {
-        initStorage(n);
-
-        std::random_device rdev;
-        std::mt19937 generator(rdev());
-
-        std::uniform_real_distribution<> e_dist(-0.5e9, 0.5e9);
-        std::uniform_real_distribution<> ph_dist(0, 2*cnst::pi);
-        int count = 0;
-        const T sep = separatrix<T>(mAcc);
-        while (count < n) {
-            const T deltaE = e_dist(generator);
-            const T phase = ph_dist(generator);
-            const T H = hamiltonian(mAcc, deltaE, phase);
-            if ((sep - 1e6) < H && H < (sep + 1e6)) {
-                mParticles->momentum[count] = deltaE;
-                mParticles->phase[count] = phase;
-                count++;
-            }
-        }
-        writeSingleDistribution(STARTDIST_FILE);
-    }
-
-    SimpleSynchrotron(int N, RAMP_TYPE type, ActionValues)
-        : mAcc(Acc::getLHC()), mParticles(ParticlesPtr(new Particles(N))), mType(type)
-    {
-        const T sep = separatrix(mAcc);
-        //std::vector<T> d_actions = {-1e4, -9e3, -8e3, -7e3, -6e3, -5e3, -4e3, -3e3, -2e3, -1e3, -100};
-        std::vector<T> d_actions;
-        
-        int outside = 80;
-        double maxdE = 1.6e9;
-        double de = maxdE/double(outside);
-        for (int i = 1; i <= outside; ++i) {
-            d_actions.push_back(hamiltonian(mAcc, de*double(i), cnst::pi));
-        }
-        
-        int inside = 20;
-        for (int i = 1; i <= inside; ++i) {
-            d_actions.push_back(T(-8000 + 500*i));
-        }
-
-        const int n = N/(2*d_actions.size());
-        initStorage(2*n*d_actions.size());
-
-        int count = 0;
-        std::random_device rdev;
-        std::mt19937 generator(rdev());
-        for (T action : d_actions) {
-            action += sep;
-            std::uniform_real_distribution<> dist(0.0, 2*cnst::pi);
-            for (T sign : std::vector<T>({-1.0, 1.0})) {
-                for (int i = 0; i < n; ++i) {
-                    const T phase = dist(generator);
-                    const T energy = levelCurve(mAcc, phase, action, sign);
-                    if (std::isnan(energy)) { --i; continue; }
-                    mParticles->momentum[count] = energy;
-                    mParticles->phase[count] = phase;
-                    ++count;
-                }
-            }
-        }
-        std::cout << "Tried to create " << N << " particles, only initialized " << size() << std::endl;
-        writeSingleDistribution(STARTDIST_FILE);
-    }
-
-    SimpleSynchrotron(int n, RAMP_TYPE type, LinearDecay)
-        : mAcc(Acc::getLHC()), mParticles(ParticlesPtr(new Particles(n))), mType(type)
-    {
-        initStorage(n);
-        const T sep = separatrix(mAcc);
-        std::random_device rdev;
-        std::mt19937 generator(rdev());
-
-        // Distribution as (relative to separatrix)
-        //  -15k        -10k         +5k
-        //    | constant  | linear dec.|
-        std::vector<T> Hs{sep - 15e3, sep - 10e3, sep + 5e3, sep + 2.7e7};
-        std::vector<T> prob{100.0, 100.0, 0.1, 0.1};
-        std::piecewise_linear_distribution<> H_dist(Hs.begin(), Hs.end(), prob.begin());
-
-        std::uniform_real_distribution<> uni_dist(0.0, 2*cnst::pi);
-
-        for (int i = 0; i < n; ++i) {
-            const T phase = uni_dist(generator);
-            const T sign = uni_dist(generator) < cnst::pi ? 1.0 : -1.0;
-            const T action = H_dist(generator);
-            const T energy = levelCurve(mAcc, phase, action, sign);
-            if (std::isnan(energy)) { --i; continue; }
-            mParticles->momentum[i] = energy;
-            mParticles->phase[i] = phase;
-        }
-        std::cout << "Initialized " << size() << " particles" << std::endl;
-        writeSingleDistribution(STARTDIST_FILE);
-    }
-
-    SimpleSynchrotron(int n, RAMP_TYPE type, ExponentialDecay)
-        : mAcc(Acc::getLHC()), mParticles(ParticlesPtr(new Particles(n))), mType(type)
-    {
-        initStorage(n);
-
-        std::random_device rdev;
-        std::mt19937 generator(rdev());
-
-        std::exponential_distribution<> H_dist(0.0003);
-        std::uniform_real_distribution<> uni_dist(0.0, 2*cnst::pi);
-
-        const T H_low = separatrix(mAcc) - 15000;
-        for (int i = 0; i < n; ++i) {
-            const T phase = uni_dist(generator);
-            const T sign = uni_dist(generator) < cnst::pi ? 1.0 : -1.0;
-            const T action = H_low + H_dist(generator);
-            const T energy = levelCurve(mAcc, phase, action, sign);
-            if (std::isnan(energy)) { --i; continue; }
-            mParticles->momentum[i] = energy;
-            mParticles->phase[i] = phase;
-        }
-        std::cout << "Initialized " << size() << " particles" << std::endl;
-        writeSingleDistribution(STARTDIST_FILE);
-    }
-
-    SimpleSynchrotron(SixTrackTest, T energy)
-        : mAcc(Acc::getLHC()), mParticles(ParticlesPtr(new Particles(1))), mType(LHC_RAMP)
-    {
-        mParticles->momentum[0] = energy;
-        mParticles->phase[0] = cnst::pi;
-    }
-
-    void initStorage(int n)
-    {
-        mCollHits.assign(n, -1);
-    }
-
 
     //
     // FILE IO
     //
 
-    void readDistribution(std::string filePath)
-    {
-        std::cout << "Reading distribution from '" << filePath << "'" << std::endl;
-        std::ifstream file(filePath.c_str());
-        if (!file.is_open())
-            throw std::runtime_error("could not open file");
+    //void readDistribution(std::string filePath)
+    //{
+        //std::cout << "Reading distribution from '" << filePath << "'" << std::endl;
+        //std::ifstream file(filePath.c_str());
+        //if (!file.is_open())
+            //throw std::runtime_error("could not open file");
 
-        using common::skip;
+        //using common::skip;
 
-        int n;
-        file >> n >> skip;
-        initStorage(n);
+        //int n;
+        //file >> n >> skip;
+        //initStorage(n);
     
-        for (int i = 0; i < n; ++i) {
-            T de, phase;
-            file >> de >> skip<char> >> phase >> skip; // we need to consume end of line
-            mParticles->momentum[i] = de;
-            mParticles->phase[i] = phase;
-        }
-        std::cout << "Read " << size() << " particles" << std::endl;
-    }
+        //for (int i = 0; i < n; ++i) {
+            //T de, phase;
+            //file >> de >> skip<char> >> phase >> skip; // we need to consume end of line
+            //mParticles->momentum[i] = de;
+            //mParticles->phase[i] = phase;
+        //}
+        //std::cout << "Read " << size() << " particles" << std::endl;
+    //}
 
     void createTurnFileHeader(std::string filePath, int turns) const
     {
