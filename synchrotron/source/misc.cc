76ce9077efc68f1da9ac8e8c10099328613d0872
pingul <joel@wbn.se>
2017-04-10 15:34:57 +0200

    Now functioning system for combining different ramps

--- synchrotron/source/misc.cc
+++ synchrotron/source/misc.cc
@@ -5,12 +5,13 @@
 #include "settings.hh"
 #include "accelerator.hh"
 #include "hamiltonian.hh"
+#include "ramp_program.hh"
 
 int main(int argc, char* argv[])
 {
-    //using namespace twodsynch;
+    using Acc = stron::Accelerator<double>;
     std::vector<std::string> args(argv, argv + argc);
-    auto acc = twodsynch::Accelerator<double>::getLHC();
+    Acc LHC = Acc::getLHC();
 
     if (args.size() < 2) {
         std::cout << "no arguments" << std::endl;
@@ -19,9 +20,18 @@ int main(int argc, char* argv[])
         double maxdE = 1.9e9;
         double de = maxdE/double(n);
         for (int i = 0; i < n; ++i) {
-            double d = twodsynch::hamiltonian(acc, de*double(i), twodsynch::cnst::pi);
+            double d = stron::hamiltonian(LHC, de*double(i), stron::cnst::pi);
             std::cout << d << std::endl;
         }
+    } else if (args[1] == "motor") {
+        unsigned steps = 11246;
+        //auto prog = typename Program::Ptr(new stron::CollimatorRamp<Acc>(LHC, steps, "resources/motor_tcp_ir3_f5433b1.txt", 0));
+        auto prog = stron::ramp::create(LHC, steps, stron::ramp::ProgramType::LHCRamp);
+        for (int i = 0; i < steps - 1; ++i) {
+            prog->step();
+            std::cout << LHC.E() << " " << LHC.collimators[0].left << " " << LHC.collimators[0].right << std::endl;
+        }
+        
     }
 }
 
--- synchrotron/source/ramp_program.hh
+++ synchrotron/source/ramp_program.hh
@@ -6,32 +6,38 @@
 #include "settings.hh"
 
 namespace stron {
+namespace ramp {
 
-template <typename Accelerator>
+template <typename Acc>
 class Program
 {
 public:
     using Ptr = std::unique_ptr<Program>;
 
-    Program(Accelerator& acc)
-        : mAcc(acc) {}
+    Program(Acc& acc, unsigned steps)
+        : mAcc(acc), mSteps(steps) {}
     virtual ~Program() {}
-    virtual void restart() = 0;
-    virtual void step() = 0;
+    virtual void setup() {}
+    virtual void step() {}
 protected:
-    Accelerator& mAcc;
+    Acc& mAcc;
+    unsigned mSteps;
 };
 
-template <typename Accelerator, typename Program = Program<Accelerator>>
-class EnergyRamp : public Program
-{
-    using T = typename Accelerator::ValType;
 
-    size_t mIndex;
-    std::vector<T> mEnergy;
+/*
+ * Energy programs
+ *
+ */
+template <typename Acc, typename Program = Program<Acc>>
+class EnergyRamp : virtual public Program
+{
 public:
-    EnergyRamp(Accelerator& acc, std::string energyProgram, int steps)
-        : Program(acc), mIndex(0)
+    EnergyRamp(Acc& acc, unsigned steps)
+        : Program(acc, steps), mIndex(0) {}
+
+    EnergyRamp(Acc& acc, unsigned steps, std::string energyProgram)
+        : Program(acc, steps), mIndex(0)
     {
         using common::skip;
         
@@ -40,38 +46,153 @@ public:
         std::ifstream file(energyProgram);
         if (!file.is_open())
             throw std::runtime_error("Could not open file");
-        for (int i = 0; i < steps; ++i) {
+        for (int i = 0; i <= steps; ++i) {
             T data;
             file >> skip >> data;
             mEnergy.emplace_back(data*1e6);
         }
     }
-    virtual void restart() override { mIndex = 0; Program::mAcc.setE(mEnergy[mIndex], true); }
-    virtual void step() override { Program::mAcc.setE(mEnergy[++mIndex]); }
-    virtual ~EnergyRamp() {}
+
+    // Parameter passing for constructors
+    virtual void setup() override { mIndex = 0; Program::mAcc.setE(mEnergy.at(mIndex), true); }
+    virtual void step() override { Program::mAcc.setE(mEnergy.at(++mIndex)); }
+protected:
+    using T = typename Acc::ValType;
+
+    unsigned mIndex;
+    std::vector<T> mEnergy;
 };
 
-template <typename Accelerator, typename Program = Program<Accelerator>>
-class CollimatorRamp : public Program
+template <typename Acc, typename EnergyRamp = EnergyRamp<Acc>>
+class DefaultEnergyRamp : public EnergyRamp
 {
-    using T = typename Accelerator::ValType;
+    using Program = Program<Acc>;
 public:
-    CollimatorRamp(Accelerator& acc, std::string motorProgram)
-        : Program(acc)
+    DefaultEnergyRamp(Acc& acc, unsigned steps)
+        : Program(acc, steps), EnergyRamp(acc, steps, LHC_RAMP_FILE) {}
+};
+
+template <typename Acc, typename EnergyRamp = EnergyRamp<Acc>>
+class AggresiveEnergyRamp : public EnergyRamp
+{
+    using Program = Program<Acc>;
+public:
+    AggresiveEnergyRamp(Acc& acc, unsigned steps)
+        : Program(acc, steps), EnergyRamp(acc, steps)
     {
+        this->mEnergy.reserve(steps);
+        for (int i = 0; i <= steps; ++i) this->mEnergy.emplace_back(450e9 + i*1e7);
     }
-    virtual ~CollimatorRamp() {}
 };
 
-template <typename Accelerator, typename EnergyRamp = EnergyRamp<Accelerator>>
-class LHCRamp : public EnergyRamp
+
+/*
+ * Collimator programs
+ *
+ */
+template <typename Acc, typename Program = Program<Acc>>
+class CollimatorRamp : virtual public Program
 {
-    using T = typename Accelerator::ValType;
 public:
-    LHCRamp(Accelerator& acc, int steps)
-        : EnergyRamp(acc, LHC_RAMP_FILE, steps) {}
+    CollimatorRamp(Acc& acc, unsigned steps, std::string motorProgram, unsigned collIndex)
+        : Program(acc, steps), mCollIndex(collIndex) 
+    {
+        using common::skip;
+
+        std::cout << "Reading '" << motorProgram << "'" << std::endl;
+        std::ifstream coll_file(motorProgram);
+        if (!coll_file.is_open())
+            throw std::runtime_error("Could not open file");
+
+        mData.reserve(steps);
+        for (int i = 0; i <= steps; ++i) {
+            T left, right;
+            coll_file >> skip >> left >> right;
+            mData.emplace_back(std::make_pair(left, right));
+        }
+    }
+    virtual void setup() override { mIndex = 0; set(); }
+    virtual void step() override { ++mIndex; set(); }
+protected:
+    using T = typename Acc::ValType;
+
+    void set()
+    {
+        // The data is in seconds but mIndex is in turns -- we need to convert and interpolate
+        T s = mIndex/cnst::s_to_turn;
+        int i_s = s;
+        T frac = s - std::floor(s);
+
+        T left, right;
+        if (mIndex < Program::mSteps) {
+            // linear interpolation
+            left = mData.at(i_s).first*(T(1) - frac) + mData.at(i_s + 1).first*frac;
+            right = mData.at(i_s).second*(T(1) - frac) + mData.at(i_s + 1).second*frac;
+        } else {
+            left = mData.at(i_s).first;
+            right = mData.at(i_s).second;
+        }
+
+        typename Acc::Collimator& coll = Program::mAcc.collimators.at(mCollIndex);
+        coll.left = left; 
+        coll.right = right; 
+    }
+
+    unsigned mIndex;
+    unsigned mCollIndex;
+    std::vector<std::pair<T, T>> mData;
 };
 
+template <typename Acc, typename CollimatorRamp = CollimatorRamp<Acc>>
+class TCP_IR3Ramp : public CollimatorRamp
+{
+    using Program = Program<Acc>;
+public:
+    TCP_IR3Ramp(Acc& acc, unsigned steps)
+        : Program(acc, steps), CollimatorRamp(acc, steps, COLL_MOTOR_FILE, 0) {}
+};
+
+
+
+/*
+ * Combined programs
+ *
+ */
+template <typename Acc, typename ...Programs>
+class RampGenerator : public Programs...
+{
+    using Base = Program<Acc>;
+public:
+    RampGenerator(Acc& acc, unsigned steps) : Base(acc, steps), Programs(acc, steps)... {} 
+    
+    virtual void setup() override { int dummy[] = {0, (Programs::setup(), void(), 0)...}; static_cast<void>(dummy); }
+    virtual void step() override { int dummy[] = {0, (Programs::step(), void(), 0)...}; static_cast<void>(dummy); }
+};
+
+enum ProgramType
+{
+    NoRamp,
+    LHCRamp,
+    AggressiveRamp,
+};
+
+template <typename Acc>
+typename Program<Acc>::Ptr create(Acc& acc, unsigned steps, ProgramType type)
+{
+    using Ptr = typename Program<Acc>::Ptr;
+    switch (type)
+    {
+        case NoRamp:
+            return Ptr(new Program<Acc>(acc, steps));
+        case LHCRamp:
+            return Ptr(new RampGenerator<Acc, DefaultEnergyRamp<Acc>, TCP_IR3Ramp<Acc>>(acc, steps));
+        default:
+            throw std::runtime_error("ProgramType not supported");
+    }
+}
+
+
+} // namespace ramp
 } // namespace stron
 
 #endif
