cfafba1f37d9dd5756b13c2a927c24515bd96225
pingul <joel@wbn.se>
2017-07-20 12:30:00 +0200

    These files must have accidentally been added

--- synchrotron/analysis/test.py
+++
@@ -1,4 +0,0 @@
-import phasespace as ps
-
-pps = ps.PhaseSpace.from_non_collimat_input("/Users/swretbor/Workspace/work_afs/sixtrack/simulations/oml_study/v7/run0001/dist0.txt")
-pps.plot_particles()
